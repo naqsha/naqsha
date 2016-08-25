4135286fe27692b1b153ccaadc1d35af198b7ef3
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-08-25 18:19:49 +0530

    included name feature into the main Feature module.

--- Naksha/Feature.hs
+++ Naksha/Feature.hs
@@ -3,10 +3,34 @@
 module Naksha.Feature
        (
          module Naksha.Feature.Internal
-         -- ** Supported features.
-       , module Naksha.Feature.Name
+         -- ** Supported Features
+       , Name(..)
+         -- ** Feature constructor.
+       , name, names
        ) where
 
 
+import           Data.Text
+import           Data.Typeable( Typeable )
+
 import Naksha.Feature.Internal
-import Naksha.Feature.Name
+
+---------  Names of object -------------------------------------------
+
+-- | The language code use to distinguish names in different languages.
+type Language = Text
+
+-- | A name of an object. Objects often have multiple names in different languages. In such case,
+-- there is a canonical name as well its names in various languages.
+data Name = Name { canonicalName :: Text              -- ^ The canonical name of the object
+                 , otherNames    :: [(Language,Text)] -- ^ other (multi-lingual) names.
+                 } deriving Typeable
+
+-- | Construct the feature of type `Name`.
+name  :: Text -> Feature
+name  = flip names []
+
+-- | Construct the feature of type `Name` having possibly many names
+-- in different languages.
+names  :: Text -> [(Language, Text)] -> Feature
+names t = Feature . Name t
