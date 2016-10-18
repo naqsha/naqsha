711fbfed1eb42e21021d031dec7aa4936c7601bc
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-10-18 11:50:03 +0530

    better attribute management with lenses.

--- Naksha/Annotate.hs
+++ Naksha/Annotate.hs
@@ -2,143 +2,98 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE FlexibleContexts          #-}
 {-# LANGUAGE ConstraintKinds           #-}
+{-# LANGUAGE RankNTypes                #-}
+{-# LANGUAGE TemplateHaskell           #-}
 
 -- | Module to annotate objects with semantic features.
 module Naksha.Annotate
        ( -- * Annotated objects.
          -- $annotate$
 
-
          -- ** Setting attributes, inspecting fields and extracting features.
          --
          -- $extract$
-         Annotated, annotate, unAnnotate, (.=), (.>), extract
+         Annotated, value, annotation, feature
+       , AttributeLens, (@=), (@?)
        , IsFeature
-
        ) where
 
+import           Control.Category
+import           Control.Lens
 import           Control.Applicative
 import qualified Data.Map       as Map
-import           Data.Monoid    hiding ((<>))
-import           Data.Semigroup
 import           Data.Typeable
 
 
-import           Prelude  -- To avoid redundunt import warnings.
+import           Prelude        hiding ((.)) -- To avoid redundunt import warnings.
 
 
 -- $annotate$
 --
 -- Geographic objects like geo-positions, trails, routes etc in actual
--- maps comes with annotations. Such objects are captured by the type
--- `Annotated`.  An object can be annotated with any type that is an
--- constrained by the constraint type `IsFeature`. It is required to
--- be:
---
--- [Typeable:] Required for efficient storing and extracting from
--- feature sets and extracting when required
---
--- [Semigroup:] When an object is annotated with the same feature type
--- multiple times, the semi-group instance is used to summarise the
--- multiple annotations.
---
--- [Show:] Features should be printable.
-
--- $extract$
---
--- We disallow direct access to the feature set of an annotated
--- object. However we can work with the annotations as follows.
---
--- 1. An object is annotated using the idiom
---
--- >
--- > annotate x [attr1 .= v2, attr2 .= v2]
--- >
---
--- where each of @attr1@, @attr2@ are _attributes_ to be set. For a
--- feature type @t@, any function of type @v -> t@ is an attribute
--- that takes a value @v@ and sets the feature @t@.
---
--- 2. One can extract any _field_ of an annotated object using the
---    idiom
---
--- >
--- > a .> field
--- >
---
--- A field is any function from @t -> a@ where t is a feature.
---
--- 3. One can use the function `extract` to actually extract a feature.
---
-
-
-------------------------------------- Annotated elements ---------------------------------------
-
--- | An annotated type.
-data Annotated a = Annotated { unAnnotate :: a
-                             , featureSet :: FeatureSet
-                             } deriving Show
-
--- | An @v@ valued attribute is nothing but a function from @v@ to some feature @t@. This operator
--- simulates the assignment of an attribute.
-(.=) :: IsFeature t
-     => (v -> t)        -- ^ attribute
-     -> v               -- ^ value to set for the attribute
-     -> FeatureSet
-(.=) attr v = FeatureSet $ Map.singleton key $ Feature ftr
-  where key = typeOf $ ftr
-        ftr = attr v
-
--- | Annotate an element with the given feature sets.
-annotate :: a -> [FeatureSet] -> Annotated a
-annotate a = Annotated a . mconcat
-
--- | Recover a feature from an annotated element if set.
-extract :: IsFeature t => Annotated a -> Maybe t
-extract = extractFrom . featureSet
-
--- | Let @t@ be a feature type, then a field of a feature is an arbitrary function from @t -> a@. This
--- operator can be used to extract a field of a
-(.>) :: IsFeature t => Annotated a -> (t -> b) -> Maybe b
-(.>) ann field = field <$> extract ann
+-- maps comes with annotations. Annotations on an object is captured
+-- by the `Annotation` type and an annotated object of type @a@ is
+-- captured by the the type @`Annotated` a@. Besides standard
+-- annotations the library supports annotation by any custom feature
+-- type. The only restriction is that it be an instance of
+-- `IsFeature`.
 
 ----------------------- Features and Feature set -----------------------------------------------
 
 -- | Constraint type capturing types that are allowed to be used as
 -- features.
-type IsFeature t = (Semigroup t, Typeable t, Show t)
+type IsFeature t = (Typeable t, Show t)
 
 -- | A feature is nothing but an existentially quantified `Typeable` value.
-data  Feature = forall t . IsFeature t => Feature t
+data Feature = forall t . IsFeature t => Feature t
+
+type FeatureMap = Map.Map TypeRep Feature
 
 instance Show Feature where
   show (Feature t) = show t
 
--- | A set of features. Extracting features from this list is fast. Feature sets
--- are monoids where the monoid operation combines the set of
--- features.
-newtype FeatureSet = FeatureSet (Map.Map TypeRep Feature)
+------------------------------------- Annotated elements ---------------------------------------
 
-instance Show FeatureSet where
-  show (FeatureSet fs) = show $ Map.elems fs
-
-instance Semigroup Feature where
-  (<>) (Feature t1) (Feature t2) = Feature $ maybe t1 comb mt2
-    where mt2  = cast t2 `asTypeOf` Just t1
-          comb = (t1<>)
-
-instance Semigroup FeatureSet where
-  (<>) = mappend
-
-instance Monoid FeatureSet where
-  mempty  = FeatureSet Map.empty
-  mappend (FeatureSet f1) (FeatureSet f2) = FeatureSet $ Map.unionWith (<>) f1 f2
+-- | An annotated type.
+data Annotated a = Annotated { _value      :: a
+                             , _annotation :: FeatureMap
+                             } deriving Show
 
 
--- | Extract a given feature out of the feature set.
-extractFrom :: IsFeature t => FeatureSet -> Maybe t
-extractFrom = extractFromP undefined
-  where extractFromP :: Typeable t => t -> FeatureSet -> Maybe t
-        extractFromP t (FeatureSet s) = Map.lookup (typeOf t) s >>= castFeature undefined
-        castFeature :: Typeable t => t -> Feature -> Maybe t
-        castFeature _ (Feature x) = cast x
+
+makeLenses ''Annotated
+
+instance Functor Annotated where
+  fmap f (Annotated a ann) = Annotated (f a) ann
+
+instance Applicative Annotated where
+  pure = flip Annotated Map.empty
+  (Annotated f _) <*> (Annotated x ann) =  Annotated (f x) ann
+
+
+type AttributeLens a v = Lens' (Annotated a) (Maybe v)
+
+-- | Lens to look into a custom feature of the annotation.
+feature :: IsFeature t => AttributeLens a t
+feature = annotation . featureSetLens
+  where castFeature (Just (Feature x)) = cast x
+        castFeature _                  = Nothing
+
+        featureLens :: IsFeature t => Lens' (Maybe Feature) (Maybe t)
+        featureLens = lens castFeature $ \ s -> maybe s (Just . Feature)
+        featureSetLens = fsLens undefined
+          where fsLens :: IsFeature t => t -> Lens' FeatureMap (Maybe t)
+                fsLens t = at (typeOf t) . featureLens
+
+
+-- | Set a custom feature.
+(@=) :: IsFeature t
+     => Iso' t v      -- ^ the attribute to set (ig
+     -> v             -- ^ the attribute value
+     -> Annotated a
+     -> Annotated a
+(@=) isom v =  feature .~ Just (view (from isom) v)
+
+-- | Query a custom feature.
+(@?) :: IsFeature t => Annotated a -> Lens' t v -> Maybe v
+annA @? lnz = annA ^? feature . _Just . lnz
--- Naksha/Feature.hs
+++ Naksha/Feature.hs
@@ -1,57 +1,43 @@
 {-# LANGUAGE DeriveDataTypeable         #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
--- | This module captures features of objects.
-module Naksha.Feature
-       ( Name(..), I8NNames, nameIn, nameInLanguage
-       , Description(..), Comment(..)
-       , Elevation(..)
-       , LinkSet(..), link
-       ) where
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE Rank2Types                 #-}
 
-import qualified Data.Map                   as M
-import           Data.Semigroup
+-- | This module captures features of objects.
+module Naksha.Feature where
+
+
+import           Control.Lens
+import           Data.Map                  as Map
 import           Data.String
+import           Data.Semigroup
 import           Data.Text                  as T
 import           Data.Typeable         ( Typeable )
 
-
 import           Naksha.Language
-import           Naksha.Annotate
 
----------  Names of object -------------------------------------------
-
-newtype Name = Name {name :: Text} deriving (Typeable, Show)
-
-
-instance Semigroup Name where
-  (<>) _ n = n
-
--- | The set of multi-lingual names.
-newtype I8NNames = I8NNames { i8nNames :: M.Map Language Text } deriving (Typeable, Show)
-
--- | Construct the feature of type `Name` having possibly many names
--- in different languages.
-nameIn       :: Language
-             -> Text
-             -> I8NNames
-nameIn lang  =  I8NNames . M.singleton lang
-
-nameInLanguage :: Language -> Annotated a -> Maybe Text
-nameInLanguage lang a = (a .> i8nNames) >>= M.lookup lang
-
-instance Semigroup I8NNames where
-  (<>) n1 n2 = I8NNames $ i8nNames n1 `M.union` i8nNames n2
 
 
 ----------------------- Elevation from sea level ------------------------------------
 
+newtype Name = Name { _name :: Text }  deriving (Typeable, Show, IsString)
+
+makeLenses ''Name
+
+
+-- | Multi-lingual name.
+newtype MultiName = MultiName { _multiNames :: Map Language Text } deriving (Typeable, Show, Monoid)
+
+
+--
+makeLenses ''MultiName
 
 -- | Elevation form mean sea level in metres.
-newtype Elevation = Elevation { elevation :: Double } deriving (Typeable, Show)
+newtype Elevation = Elevation { _elevation :: Double } deriving (Typeable, Show, Num, Fractional)
 
-instance Semigroup Elevation where
-  (<>) _ e = e
+makeLenses ''Elevation
+
 
 -------------------------- Links -------------------------------------------------------
 
@@ -61,22 +47,12 @@ newtype LinkSet = LinkSet { linkSet :: [Text] } deriving (Typeable, Show, Semigr
 instance IsString LinkSet where
   fromString = LinkSet . (:[]) . fromString
 
--- | A single link.
-link :: Text -> LinkSet
-link  = LinkSet . (:[])
-
 ------------------------- Description and --------- -------------------------------------
 
 -- | The description of an element.
-newtype Description = Description { description :: Text } deriving (Typeable, Show, IsString)
-
-instance Semigroup Description where
-  (<>) _ e = e
+newtype Description = Description Text deriving (Typeable, Show, IsString)
 
 -------------------------- Comment -------------------------------------------------------
 
 -- | The description of an element.
-newtype Comment = Comment { comment :: Text } deriving (Typeable, Show, IsString)
-
-instance Semigroup Comment where
-  (<>) _ e = e
+newtype Comment = Comment Text deriving (Typeable, Show, IsString)
