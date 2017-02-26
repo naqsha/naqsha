3bc17f4c4dde4c736484ba74d691201fcf444422
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-02-26 20:02:31 +0530

    added top level function for open street map.

---
+++ Naqsha/OpenStreetMap.hs
@@ -0,0 +1,7 @@
+module Naqsha.OpenStreetMap
+       ( module Naqsha.OpenStreetMap.Element
+       , module Naqsha.OpenStreetMap.Stream
+       ) where
+
+import Naqsha.OpenStreetMap.Element
+import Naqsha.OpenStreetMap.Stream
