923e7442dd65c0996246d6cad30c6cc9c3a340c9
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-03-29 18:55:29 +0530

    more property based testing for latitude and longitude.

--- tests/Naqsha/PositionSpec.hs
+++ tests/Naqsha/PositionSpec.hs
@@ -1,26 +1,60 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Naqsha.PositionSpec where
-
+import Data.Monoid
+import Test.QuickCheck
 import Test.Hspec
+import Test.Hspec.QuickCheck
 
 import Naqsha.Position
+import Naqsha.Arbitrary ()
+
+inRange :: Testable prop
+        => (Int, Int)    -- ^ Range
+        -> String        -- ^ description
+        -> ((Int,Int) -> prop)
+        -> Spec
+inRange rng descr prop = it label $ forAll pair prop
+  where pair = (,) <$> choose rng <*> choose rng
+        label = "in range " ++ show rng ++ ": " ++ descr
+
+
+isIncreasing :: Angular a => a -> (Int, Int) ->  Bool
+isIncreasing a (x , y) | x == y = toDeg xA == toDeg yA
+                       | x <  y = toDeg xA <  toDeg yA
+                       | x >  y = toDeg xA >  toDeg yA
+  where xA = intToAngular x
+        yA = intToAngular y
+        intToAngular = flip asTypeOf a . deg . toEnum
+
+isDecreasing :: Angular a => a -> (Int, Int) -> Bool
+isDecreasing a (x, y) | x == y = toDeg xA == toDeg yA
+                      | x >  y = toDeg xA <  toDeg yA
+                      | x <  y = toDeg xA >  toDeg yA
+  where xA = intToAngular x
+        yA = intToAngular y
+        intToAngular = flip asTypeOf a . deg . toEnum
 
 
 spec :: Spec
 spec = do
   describe "latitudes" $ do
-    it "should increase till 90"
-      $ deg 90  `shouldBe` (deg 90  :: Latitude)
-    it "should decrease after 90"
-      $ deg 91  `shouldBe` (deg 89  :: Latitude)
-    it "should become zero at 180"
-      $ deg 180 `shouldBe` (deg 0  :: Latitude)
-    it "should become negative beyond 180"
-      $ deg 181   `shouldBe` (deg (-1) :: Latitude)
-    it "should decrease till -90"
-      $ deg (-90)   `shouldBe` (deg (-90)  :: Latitude)
-    it "should increase after -90"
-      $ deg (-91)   `shouldBe` (deg (-89)  :: Latitude)
-    it "should become zero at -180"
-      $ deg (-180) `shouldBe` (deg 0  :: Latitude)
-    it "should become positive beyond -180"
-      $ deg (-181) `shouldBe` (deg 1 :: Latitude)
+    prop "show have a period of 360 deg" $ \ (x :: Latitude) ->
+      x <> deg 360 == x
+
+    prop "normalised latitude lie in -90 to 90" $ \ (x :: Latitude) ->
+      let xDeg = toDeg (normalise x)
+          in xDeg >= -90 && xDeg <= 90
+
+    inRange (-90,90) "latitude increases" $ isIncreasing (undefined :: Latitude)
+    inRange (90, 270) "latitude decreases" $ isDecreasing (undefined :: Latitude)
+
+
+  describe "longitudes" $ do
+    prop "should have a period of 360 deg" $ \ (x :: Longitude) ->
+      x <> deg 360 == x
+
+    prop "normalised longitude lie in -180 to 180" $ \ (x :: Longitude) ->
+      let xDeg = toDeg (normalise x)
+          in xDeg >= -180 && xDeg <= 180
+
+    inRange (-180, 180) "longitude increases" $ isIncreasing (undefined :: Longitude)
