8bcbdcbda8bd03809591902ee38aa6ebba355f43
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-04-04 13:12:00 +0530

    expose Language module from OpenStreetMap.

--- Naqsha/OpenStreetMap.hs
+++ Naqsha/OpenStreetMap.hs
@@ -1,5 +1,7 @@
 module Naqsha.OpenStreetMap
        ( module Naqsha.OpenStreetMap.Element
+       , module Naqsha.OpenStreetMap.Language
        ) where
 
 import Naqsha.OpenStreetMap.Element
+import Naqsha.OpenStreetMap.Language hiding ( lang )
