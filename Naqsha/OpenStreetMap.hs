2bf16070517b094e7659ff6e17484f27e4594109
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-04-04 13:59:34 +0530

    more careful exposing of stuff from top level module.

--- Naqsha/OpenStreetMap.hs
+++ Naqsha/OpenStreetMap.hs
@@ -1,12 +1,15 @@
 module Naqsha.OpenStreetMap
        ( module Naqsha.OpenStreetMap.Element
        , module Naqsha.OpenStreetMap.Language
+       , module Naqsha.OpenStreetMap.Tags
        ) where
 
 import Naqsha.OpenStreetMap.Element hiding( OsmMeta
                                           , _osmID, _modifiedUser, _modifiedUserID
                                           , _timeStamp, _changeSet, _version, _isVisible
+                                          , osmID, modifiedUser, modifiedUserID
+                                          , timeStamp, changeSet, version, isVisible
                                           )
 
-import Naqsha.OpenStreetMap.Language hiding ( lang )
+import Naqsha.OpenStreetMap.Language hiding ( Language, lang )
 import Naqsha.OpenStreetMap.Tags
