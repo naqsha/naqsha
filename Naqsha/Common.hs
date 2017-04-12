8835da1acfe5e11a3fcdd15803194bb2e1fbce67
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-04-12 20:47:55 +0530

    added convenient functions to build and change elements.

--- Naqsha/Common.hs
+++ Naqsha/Common.hs
@@ -3,12 +3,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Naqsha.Common
        ( showT, readMaybeT, showVersionT, naqshaVersionT
-       , showTime, timeParser, osmXmlVersion
+       , showTime, timeParser, osmXmlVersion, buildM, build, withChanges, withChangesM
        ) where
 
-import Data.Time
+import Control.Monad.State
+import Data.Default
 import Data.Monoid
 import Data.Text       ( Text, pack, unpack       )
+import Data.Time
 import Data.Version    ( Version(..), showVersion )
 import Text.Read       ( readMaybe                )
 import Paths_naqsha as NP
@@ -52,3 +54,20 @@ showVersionT = pack . showVersion
 
 naqshaVersionT :: Text
 naqshaVersionT = "naqsha-" <> showVersionT NP.version
+
+
+-- | Building an element given a set field assignment.
+buildM :: (Monad m, Default d) => StateT d m () -> m d
+buildM = withChangesM def
+
+-- | A pure version of `buildM`.
+build :: Default d => State d () -> d
+build = withChanges def
+
+-- | Changes to an element.
+withChangesM :: Monad m => d -> StateT d m () -> m d
+withChangesM = flip execStateT
+
+-- | A pure version of `withChangesM`.
+withChanges :: d -> State d () -> d
+withChanges = flip execState
