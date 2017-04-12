8707c1c6c9749946e9470fa9bcb28cae03dd76bc
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-04-12 21:06:42 +0530

    cleanup the docs of tags module.

--- Naqsha/OpenStreetMap/Tags.hs
+++ Naqsha/OpenStreetMap/Tags.hs
@@ -1,23 +1,35 @@
 {-# LANGUAGE Rank2Types        #-}
 {-# LANGUAGE OverloadedStrings #-}
--- | This module exposes some standard tags use in the open street
--- map. We give a lens based interface for setting and reading tags of
--- open street map elements. The lenses defined here gives a more type
--- safe to set and access the tags than directly using `tagAt`.
+
+-- | This module exposes some standard tags use in the Open Street Map
+-- project. We give a lens based interface for setting and reading tags
+-- of open street map elements. The lenses defined here gives a more
+-- type safe to set and access the tags than directly using `tagAt`.
 --
 -- TODO: There are a huge number of standard tags supported by Open
 -- Street Map that are not yet present here. Please contribute towards
 -- expanding the list here.
+
 module Naqsha.OpenStreetMap.Tags
-       ( name, nameIn, elevation
+       (
+       -- $tags$
+       name, elevation
+       -- * Multilingual.
+       -- $multilingual$
+       , nameIn
+       , module Naqsha.OpenStreetMap.Language
        ) where
 
 import Control.Lens
 import Data.Monoid
-import Data.Text                    (Text)
+import Data.Text                            ( Text     )
+
 import Naqsha.Common
 import Naqsha.OpenStreetMap.Element
 import Naqsha.OpenStreetMap.Language
+
+
+
 
 
 fromTagLens :: (Show a, Read a)
@@ -31,10 +43,16 @@ fromTagLens lenz = lenz . lens toA fromA
 name :: OsmTagged e => Lens' e (Maybe Text)
 name = tagAt "name"
 
--- | Lens to focus on the name in a given language.
-nameIn :: OsmTagged e => Language -> Lens' e (Maybe Text)
-nameIn (Language l) = tagAt $ "name:" <> l
-
 -- | Lens to focus on the elevation (in meters).
 elevation :: OsmTagged e => Lens' e (Maybe Double)
 elevation = fromTagLens $ tagAt $ "ele"
+
+----------------------- Multi-lingual names -----------------
+
+-- $multilingual$
+--
+-- Open street map has ways to provide multi-lingual names to objects.
+-- Using the `nameIn` lens to set or access the multi-lingual names.
+-- | Lens to focus on the name in a given language.
+nameIn :: OsmTagged e => Language -> Lens' e (Maybe Text)
+nameIn (Language l) = tagAt $ "name:" <> l
