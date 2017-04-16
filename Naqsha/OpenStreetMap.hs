15a44f62ea2d5a41356932ba49988e73fbd39c30
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-04-16 18:39:38 +0530

    docs on the top level osm module.

--- Naqsha/OpenStreetMap.hs
+++ Naqsha/OpenStreetMap.hs
@@ -1,5 +1,11 @@
+-- | This is the top level module that needs to be imported to work
+-- with open street map. We expose the basic elements, tags and meta
+-- information from here. We also expose a streaming interface for
+-- elements form this module.
+--
 module Naqsha.OpenStreetMap
        ( module Naqsha.OpenStreetMap.Element
+       , module Naqsha.OpenStreetMap.Stream
        , module Naqsha.OpenStreetMap.Tags
        , module Naqsha.Position
        ) where
@@ -7,8 +13,8 @@ module Naqsha.OpenStreetMap
 import Naqsha.OpenStreetMap.Element hiding( OsmMeta
                                           , _osmID, _modifiedUser, _modifiedUserID
                                           , _timeStamp, _changeSet, _version, _isVisible
-                                          , osmID, modifiedUser, modifiedUserID
-                                          , timeStamp, changeSet, version, isVisible
                                           )
+
+import Naqsha.OpenStreetMap.Stream
 import Naqsha.OpenStreetMap.Tags
 import Naqsha.Position
