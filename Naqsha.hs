0885156e5d83b8ef140fb3e68fd0344a40fe8c0a
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-04-20 20:06:38 +0530

    missing top level module added.

---
+++ Naqsha.hs
@@ -0,0 +1,9 @@
+-- | Naqsha is a library to work with geo data. This module exposes
+-- the basic types and some utility functions.
+module Naqsha
+       ( module Naqsha.Position
+       , module Naqsha.Geometry.Angle
+       ) where
+
+import Naqsha.Position
+import Naqsha.Geometry.Angle
