b39edd130b926d66944cb44973c8f6ffc28147ef
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-05-17 13:01:05 +0530

    updated tests of geohash.

--- tests/Naqsha/Geometry/Coordinate/GeoHashSpec.hs
+++ tests/Naqsha/Geometry/Coordinate/GeoHashSpec.hs
@@ -1,16 +1,18 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Naqsha.Geometry.Coordinate.GeoHashSpec where
-import Data.Monoid
-import Data.Fixed
 import Test.QuickCheck
 import Test.Hspec
 import Test.Hspec.QuickCheck
 
 import Naqsha.Geometry.Coordinate
 import Naqsha.Geometry.Coordinate.GeoHash as GeoHash
-import Naqsha.Geometry.Angle
 import Naqsha.Arbitrary ()
+
+
+
+geoHashRange :: Gen Geo
+geoHashRange = suchThat arbitrary ( \ g -> g /= northPole && g /= southPole)
 
 spec :: Spec
 spec = describe "decode-encode laws" $ do
-  prop "decode . encode should be id" $ \ g -> GeoHash.decode (GeoHash.encode g) `shouldBe` g
+  prop "decode . encode should be id" $ forAll geoHashRange $ \ g -> GeoHash.decode (GeoHash.encode g) `shouldBe` g
