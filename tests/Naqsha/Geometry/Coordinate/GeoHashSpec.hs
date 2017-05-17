d0ffb20f2aca9cc9809c1078ea01e3a996593624
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-05-17 19:34:03 +0530

    cleanup tests for geoHash.

--- tests/Naqsha/Geometry/Coordinate/GeoHashSpec.hs
+++ tests/Naqsha/Geometry/Coordinate/GeoHashSpec.hs
@@ -1,28 +1,30 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE Rank2Types          #-}
 module Naqsha.Geometry.Coordinate.GeoHashSpec where
 import Test.QuickCheck
 import Test.Hspec
 import Test.Hspec.QuickCheck
 
-import Naqsha.Geometry.Coordinate
 import Naqsha.Geometry.Coordinate.GeoHash as GeoHash
-import Naqsha.Arbitrary ()
+import Naqsha.Arbitrary
 
 
 
-geoHashRange :: Gen Geo
-geoHashRange = suchThat arbitrary ( \ g -> g /= northPole && g /= southPole)
-
-
-encodeOrderSpec :: Spec
-encodeOrderSpec = prop "preserves ordering under bytstring encoding" $
-                  forAll arbitraryPair $ \ (g1,g2)
-                                         -> compare g1 g2 `shouldBe` compare (toByteString g1) (toByteString g2)
-  where arbitraryGeoHash = encode <$> geoHashRange
-        arbitraryPair   = (,) <$> arbitraryGeoHash <*> arbitraryGeoHash
+preserves :: (Eq a, Show a)
+          => String
+          -> (forall b. Ord b => b -> b -> a)
+          -> Spec
+preserves name ordF  =
+  prop mesg $ \ (g1 :: GeoHash) g2  -> ordF g1 g2 `shouldBe` ordF (toByteString g1) (toByteString g2)
+  where mesg = "preserves " ++ name
 
 
 spec :: Spec
-spec = describe "decode-encode laws" $ do
+spec = do
   prop "decode . encode should be id" $ forAll geoHashRange $ \ g -> GeoHash.decode (GeoHash.encode g) `shouldBe` g
-  encodeOrderSpec
+
+  describe "toByteString" $ do
+    preserves  "compare" compare
+    preserves  "<="  (<=)
+    preserves  "<"   (<)
+    preserves  ">="  (>=)
