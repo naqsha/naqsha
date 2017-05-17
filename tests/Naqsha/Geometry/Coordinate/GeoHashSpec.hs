a442c3bd9588c378e174016635a3450397e4ba8f
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-05-17 14:20:44 +0530

    added test for checking the encoding order of geohash.

--- tests/Naqsha/Geometry/Coordinate/GeoHashSpec.hs
+++ tests/Naqsha/Geometry/Coordinate/GeoHashSpec.hs
@@ -13,6 +13,16 @@ import Naqsha.Arbitrary ()
 geoHashRange :: Gen Geo
 geoHashRange = suchThat arbitrary ( \ g -> g /= northPole && g /= southPole)
 
+
+encodeOrderSpec :: Spec
+encodeOrderSpec = prop "preserves ordering under bytstring encoding" $
+                  forAll arbitraryPair $ \ (g1,g2)
+                                         -> compare g1 g2 `shouldBe` compare (toByteString g1) (toByteString g2)
+  where arbitraryGeoHash = encode <$> geoHashRange
+        arbitraryPair   = (,) <$> arbitraryGeoHash <*> arbitraryGeoHash
+
+
 spec :: Spec
 spec = describe "decode-encode laws" $ do
   prop "decode . encode should be id" $ forAll geoHashRange $ \ g -> GeoHash.decode (GeoHash.encode g) `shouldBe` g
+  encodeOrderSpec
