6825e4cb708d17f478486fdf44914fba4c7c90d5
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-07-06 20:15:53 +0530

    added tests for geohash.

--- tests/Naqsha/Geometry/Coordinate/GeoHashSpec.hs
+++ tests/Naqsha/Geometry/Coordinate/GeoHashSpec.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE Rank2Types          #-}
 module Naqsha.Geometry.Coordinate.GeoHashSpec where
+import Data.String
 import Test.QuickCheck
 import Test.Hspec
 import Test.Hspec.QuickCheck
@@ -10,21 +11,7 @@ import Naqsha.Arbitrary
 
 
 
-preserves :: (Eq a, Show a)
-          => String
-          -> (forall b. Ord b => b -> b -> a)
-          -> Spec
-preserves name ordF  =
-  prop mesg $ \ (g1 :: GeoHash) g2  -> ordF g1 g2 `shouldBe` ordF (toByteString g1) (toByteString g2)
-  where mesg = "preserves " ++ name
-
-
 spec :: Spec
 spec = do
-  prop "decode . encode should be id" $ forAll geoHashRange $ \ g -> GeoHash.decode (GeoHash.encode g) `shouldBe` g
-
-  describe "toByteString" $ do
-    preserves  "compare" compare
-    preserves  "<="  (<=)
-    preserves  "<"   (<)
-    preserves  ">="  (>=)
+  prop "fromString . show = id" $ \ (g :: GeoHash) -> (fromString  $ show g) `shouldBe` g
+  prop "encode . decode  = id"  $ \ (g :: GeoHash) -> (encode $ decode g)    `shouldBe` g
