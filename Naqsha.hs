d419a5dca1c246ba52ef1fb665bea0ce6e656ace
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-07-08 12:04:37 +0530

    reorganise the top level modules
    - use the Naqsha.Geometry as the top level.

--- Naqsha.hs
+++
@@ -1,9 +0,0 @@
--- | Naqsha is a library to work with geo data. This module exposes
--- the basic types and some utility functions.
-module Naqsha
-       ( module Naqsha.Geometry.Angle
-       , module Naqsha.Geometry.Coordinate
-       ) where
-
-import Naqsha.Geometry.Angle
-import Naqsha.Geometry.Coordinate
---
+++ Naqsha/Geometry.hs
@@ -0,0 +1,62 @@
+-- | The geometric types and values exposed by naqsha.
+module Naqsha.Geometry
+       ( module Naqsha.Geometry.Coordinate
+
+       -- ** Angles and angular quantities.
+       , module Naqsha.Geometry.Angle
+
+       -- * Geometric hashing.
+       -- $geohashing$
+
+       -- * Distance calculation.
+       --
+       -- $distance$
+       --
+
+       -- * Internal details
+       -- $internals$
+       ) where
+
+import Naqsha.Geometry.Angle
+import Naqsha.Geometry.Coordinate
+
+-- Noting imported here. Only for docs.
+import Naqsha.Geometry.Spherical()
+
+
+-- $geohashing$
+--
+-- Geometric hashing is a technique of converting geometric
+-- coordinates into 1-dimension strings. Often these hashes ensures
+-- that string with large common prefix are close by (although not the
+-- converse). Hence, these hashes can be used to stored geo-cordinates
+-- in database and build into it a sense of location awareness. We support
+-- the following geometric hashing:
+--
+-- ["Naqsha.Geometry.Coordinate.GeoHash":] The geohash standard
+-- (<https://en.wikipedia.org/wiki/Geohash>).
+--
+-- None of these modules are imported by default the user may import
+-- the one that is most desirable.
+
+-- $distance$
+--
+-- Calculating quantities like distance, bearing etc depends on the
+-- model of the globe that we choose. Even in a given model we might
+-- have different algorithms to compute the distance depending on
+-- speed-accuracy trade-offs. Choosing the correct model and
+-- algorithms is application dependent and hence we do not expose any
+-- default ones. The following modules can be imported depending on the need
+--
+-- ["Naqsha.Geometry.Spherical": ] Assume a spherical model of the
+-- globe. Distance is calculated using the haversine formula.
+
+
+-- $internals$
+--
+-- The basic types like `Latitude` or `Longitude` are exposed as
+-- opaque types from this module. For type safety, we encourage the
+-- users to use this module mostly when dealing with those times. For
+-- the rare case when some non-trivial operations need to be defined,
+-- we expose the internal module "Naqsha.Geometry.Internal". However,
+-- use this interface with caution.
